6b72c40710e446504bc6f92c209c3fe2bd34bddf
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-04-14 12:42:21 +0530

    Added container for routing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
+    <div>
     <Header/>
+    <Container className='py-4'>
+        {/* You can add content here or leave it empty for now */}
+        <h1>Welcome to My React App!</h1>
+        <p>This is a simple text inside a Bootstrap container.</p>
+      </Container>
+    <Footer/>
+
+    </div>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer className='footer bg-dark text-light text-center py-3'>
+        <Container>
+            <p>&copy;  2024 Ente Gov - All rights reserved</p>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
