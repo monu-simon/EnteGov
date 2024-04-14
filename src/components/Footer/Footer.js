c9beb1d8e3251e2f03e58fc4591bd10dcfacd853
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-04-14 23:29:26 +0530

    footer changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className='footer bg-dark text-light text-center py-3'>
-        <Container>
-            <p>&copy;  2024 Ente Gov - All rights reserved</p>
-        </Container>
+      <Container>
+        <p>&copy;  2024 Ente Gov </p>
+        <a href="https://www.linkedin.com/in/monusimonm/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={24} style={{ color: 'white' }} />
+        </a>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
