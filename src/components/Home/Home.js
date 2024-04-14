8ea48c1973b207cf63f2e395028fc1ddd8d9482b
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-04-14 14:29:36 +0530

    UI changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import MemberDetails from '../MemberDetails/MemberDetails';
+import { Container } from 'react-bootstrap';
 
 function Home() {
     const [members, setMembers] = useState([]);
@@ -21,7 +22,13 @@ function Home() {
     return (
         <div>
             <h1>Member Details</h1>
+            <Container fluid>
+            <div style={{ overflowX: 'auto', overflowY: 'auto', maxHeight: '800px' }}>
             {members && <MemberDetails members={members} />}
+            </div>
+            
+            </Container>
+            
         </div>
     );
 }
