1ee01f1417e615974761f134d4dc7dd8e7e36196
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-04-14 12:49:24 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import Form from 'react-bootstrap/Form';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { Container } from 'react-bootstrap';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    <Header/>
-    <Container className='py-4'>
-        {/* You can add content here or leave it empty for now */}
-        <h1>Welcome to My React App!</h1>
-        <p>This is a simple text inside a Bootstrap container.</p>
-      </Container>
-    <Footer/>
-
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Container className="py-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
