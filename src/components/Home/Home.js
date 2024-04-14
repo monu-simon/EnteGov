de737e9cf81a70c5ff2dda727a16eadf73d71ac2
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-04-14 13:02:08 +0530

    added api call

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,27 @@
 // src/components/Home/Home.js
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
-    </div>
-  );
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+
+        const fetchMembers = async () => {
+            const apiUrl = 'https://sansad.in/api_ls/member?loksabha=17&state=&party=&gender=&ageFrom=&ageTo=&noOfTerms=&searchText=&constituency=&sitting=1&locale=en&month=&profession=&otherProfession=&constituencyCategory=&positionCode=&qualification=&noOfChildren=&isFreedomFighter=&memberStatus=s';
+            const response = await axios.get(apiUrl);
+            setMembers(response.membersDtoList)
+        };
+
+        fetchMembers();
+    },[]);
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome to the home page!</p>
+        </div>
+    );
 }
 
 export default Home;
