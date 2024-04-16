7e5abab49e76ff9ad9c93c4bcb04936298fd0cc2
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-04-16 15:51:32 +0530

    serverless

---
+++ api/party.js
@@ -0,0 +1,23 @@
+// api/party.js
+
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  try {
+    // Make a GET request to the external API
+    const response = await axios.get('https://sansad.in/api_ls/member/partyWiseRepresentation', {
+      params: {
+        loksabha: 17,
+        locale: 'en'
+        // Add other query parameters as needed
+      }
+    });
+
+    // Return the response data to the client
+    res.status(200).json(response.data);
+  } catch (error) {
+    // Handle errors
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
