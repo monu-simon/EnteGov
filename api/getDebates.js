d3eac6bed65e1ed8f7431f3a35ee01670da7ac76
Monu Simon M <monusimonm@Monus-MacBook-Air.local>
2024-04-16 16:03:55 +0530

    serverless

--- api/getDebates.js
+++ api/getDebates.js
@@ -5,7 +5,16 @@ const axios = require('axios');
 module.exports = async (req, res) => {
   const memberId = req.query.id;
   try {
-    const response = await axios.get(`https://sansad.in/api_ls/debate/debate-search?debateTypeId=&loksabha=17&sessionNumber=&fromDate=&toDate=&searchKeyword=&page=1&size=10&mpCode=${memberId}&sortBy&sortOrder`);
+    const response = await axios.get(`https://sansad.in/api_ls/debate/debate-search`,
+    {
+      params: {
+        loksabha: 17,
+        mpCode: memberId,
+        page: 1,
+        size: 10
+        // Add other query parameters as needed
+      }
+    });
     res.status(200).json(response.data);
   } catch (error) {
     console.error('Error:', error);
